4d9b404524dced958e3ae894ab92a1674921ebf6
SleepyReggy <reggy.ricardo@upsjb.edu.pe>
2024-11-19 22:13:48 -0500

    su
    sa

--- src/App.js
+++ src/App.js
@@ -8,13 +8,20 @@ function App() {
       <header className="App-header">
         <span>Informe de Datos</span>
         <div className="superior">
-
+        <h1>Proyecto</h1>
         </div>
-                
+        <div className="inferior">
+          <h2>Contenido</h2>
+        </div>
+        <footer>
+      <h4>Desarrollado : Ing: Ricardo</h4>
+      <h5>Celular:945388444</h5>
+     </footer>
+
         
       </header>
 
-     
+
 
     </div>
   );
